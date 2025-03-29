ad72c29c6e8777c8320a06af415692ed5a9ecef3
devloperpalak <palakbasak8@gmail.com>
2025-03-29 06:05:09 +0600

    removed unused robots.js file

--- src/app/api/robots.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-    res.setHeader("Content-Type", "text/plain");
-    res.send(`
-        User-agent: *
-        Disallow: ${process.env.NEXT_PUBLIC_SITE_URL || "/admin/"}
-        Allow: /
-        Sitemap: ${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml
-    `);
-}
